0972123e494afa19daf498a40577b7db17be08a8
Fenrow <45560852+Fenrow@users.noreply.github.com>
2018-12-30 23:58:03 +0100

    v0.2 +Dodanie możliwości edycji wpisu +Poprawa ustawienia "Dodaj nowy temat" na stronie

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -17,4 +17,7 @@ urlpatterns = [
     path(r'new_topic/', views.new_topic, name='new_topic'),
     #Strona odpowiedzialna za dodanie wpisu do konkretnego tematu
     re_path(r'new_entry/(?P<topic_id>\d+)/', views.new_entry, name='new_entry'),
+    #Strona pozwalająca na edycje wpisu
+    re_path(r'edit_entry/(?P<entry_id>\d+)/', views.edit_entry, name='edit_entry')
+    
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -73,3 +73,26 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
 
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edycja wpisu"""
+
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #Żądanie początkowe, wypełnienie formularza aktualną treścią wpisu
+
+        form = EntryForm(instance=entry)
+
+    else:
+        #Przekazano dane za pomocą żądania POST należy je przetworzyć
+
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
