8ec7e44938a8773a743feb500c28c6fafc372fbc
Daniel <45560852+Fenrow@users.noreply.github.com>
2019-01-02 17:18:06 +0100

    make it better v10

--- ll/settings.py
+++ ll/settings.py
@@ -160,7 +160,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
